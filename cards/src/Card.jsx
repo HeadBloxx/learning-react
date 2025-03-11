9a949d0175e5acaa0ee9c8f8b08de617e377aa7f
HeadBloxx <bloxx@gmail.com>
2025-03-11 11:59:26 +0100

    first

---
+++ cards/src/Card.jsx
@@ -0,0 +1,13 @@
+
+
+function Card(){
+    return(
+        <div className="card">
+            <img className="card-image" src="https://via.placeholder.com/150" alt="profile picture"></img>
+            <h2 className="card-title">Imrakadabra</h2>
+            <p className="card-text">I make things and go to school</p>
+        </div>
+    );
+}
+
+export default Card
